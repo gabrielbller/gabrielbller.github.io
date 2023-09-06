e0b421fda90e1657983394b8e03a3d8a2485b481
Gabriel Bodenmüller <gabrielbller@icloud.com>
2023-09-06 17:12:57 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,80 @@
-function openNav() {
-    document.getElementById("mySidepanel").style.width = "350px";
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
+
+searchBtn.addEventListener("click", () => {
+  sidebar.classList.toggle("open");
+  menuBtnChange(); 
+});
+
+function menuBtnChange() {
+  if (sidebar.classList.contains("open")) {
+    closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");
+  } else {
+    closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");
+  }
+}
+
+const productRows = document.querySelectorAll('.product-row');
+const closeModalButtons = document.querySelectorAll('.close');
+
+productRows.forEach((row) => {
+  row.addEventListener('click', (event) => {
+    const targetModalId = event.currentTarget.getAttribute('data-target');
+    const targetModal = document.getElementById(targetModalId);
+
+    if (targetModal) {
+      targetModal.style.display = 'block';
+    }
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', (event) => {
+    const modal = event.currentTarget.closest('.modal');
+
+    if (modal) {
+      modal.style.display = 'none';
+    }
+  });
+});
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  function closeNav() {
-    document.getElementById("mySidepanel").style.width = "0";
-  } 
 
-  // Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  window.onclick = function(e) {
+    if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+      if (myDropdown.classList.contains('show')) {
+        myDropdown.classList.remove('show');
+      }
+    }
   }
-}
+
+  document.addEventListener("DOMContentLoaded", function () {
+
+    var modal = document.getElementById("modal-1");
+    var returnButton = document.querySelector(".cancel-button");
+    
+
+    var telaInicial = document.getElementById("tela-inicial");
+
+    function openModal() {
+      modal.style.display = "block";
+      telaInicial.style.display = "none";
+    }
+
+    function closeModal() {
+      modal.style.display = "none";
+      telaInicial.style.display = "block";
+    }
+
+    returnButton.addEventListener("click", closeModal);
+  });
