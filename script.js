0850755e1ea142538d35b3f6ae4bad83b623ba68
gabrielbller <gabrielbller@icloud.com>
2023-11-24 17:01:30 -0300

    Atualizacoes

--- script.js
+++ script.js
@@ -7,9 +7,44 @@ document.getElementById('loadMore').addEventListener('click', function() {
   }
 });
 
+const menuAbrir = document.querySelector('.menu-barras')
 
+const nav = document.querySelector('.cabecalho')
 
+menuAbrir.addEventListener('click', () => {
+    menuAbrir.classList.toggle('aberto');
+    nav.classList.toggle('aberto');
+})
 
+const botoes = document.querySelectorAll('.cabecalho__botao');
 
+const botoesClicavel = document.querySelectorAll('.cabecalho__botao-clicavel')
 
+botoesClicavel.forEach(botao => {
+    botao.addEventListener('click', (event) => {
+        event.preventDefault()
 
+        const targetId = botao.getAttribute('href')
+        const targetElement = document.querySelector(targetId)
+
+        if(targetElement) {
+            const targetOffset = targetElement.getBoundingClientRect().top + window.scrollY;
+
+            window.scrollTo({
+                top: targetOffset,
+                behavior: 'smooth'
+            })
+        }
+    })
+})
+
+window.addEventListener("scroll", () => {
+    const barraMenu = document.querySelector('.menu-topo__fundo');
+    const scrollY = window.scrollY
+
+    if(scrollY < 50) {
+        barraMenu.style.transform = "translateY(-100%)"
+    } else {
+        barraMenu.style.transform = "translateY(0)"
+    }
+})
