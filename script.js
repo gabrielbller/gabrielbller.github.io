fb6895ea76ca7aab12950f08b6af38505115d0f3
gabrielbller <gabrielbller@icloud.com>
2023-11-24 17:03:01 -0300

    Atualizacoes

--- script.js
+++ script.js
@@ -1,50 +1,52 @@
-document.getElementById('loadMore').addEventListener('click', function() {
-  var hiddenImages = document.querySelectorAll('.portfolio__images a.hidden');
-  for (var i = 0; i < 3; i++) { // altere este número para quantas imagens você quer mostrar de cada vez
+document.getElementById("loadMore").addEventListener("click", function () {
+  var hiddenImages = document.querySelectorAll(".portfolio__images a.hidden");
+  for (var i = 0; i < 3; i++) {
+    // altere este número para quantas imagens você quer mostrar de cada vez
     if (hiddenImages[i]) {
-      hiddenImages[i].classList.remove('hidden');
+      hiddenImages[i].classList.remove("hidden");
     }
   }
 });
 
-const menuAbrir = document.querySelector('.menu-barras')
+const menuAbrir = document.querySelector(".menu-barras");
 
-const nav = document.querySelector('.cabecalho')
+const nav = document.querySelector(".cabecalho");
 
-menuAbrir.addEventListener('click', () => {
-    menuAbrir.classList.toggle('aberto');
-    nav.classList.toggle('aberto');
-})
+menuAbrir.addEventListener("click", () => {
+  menuAbrir.classList.toggle("aberto");
+  nav.classList.toggle("aberto");
+});
 
-const botoes = document.querySelectorAll('.cabecalho__botao');
+const botoes = document.querySelectorAll(".cabecalho__botao");
 
-const botoesClicavel = document.querySelectorAll('.cabecalho__botao-clicavel')
+const botoesClicavel = document.querySelectorAll(".cabecalho__botao-clicavel");
 
-botoesClicavel.forEach(botao => {
-    botao.addEventListener('click', (event) => {
-        event.preventDefault()
+botoesClicavel.forEach((botao) => {
+  botao.addEventListener("click", (event) => {
+    event.preventDefault();
 
-        const targetId = botao.getAttribute('href')
-        const targetElement = document.querySelector(targetId)
+    const targetId = botao.getAttribute("href");
+    const targetElement = document.querySelector(targetId);
 
-        if(targetElement) {
-            const targetOffset = targetElement.getBoundingClientRect().top + window.scrollY;
+    if (targetElement) {
+      const targetOffset =
+        targetElement.getBoundingClientRect().top + window.scrollY;
 
-            window.scrollTo({
-                top: targetOffset,
-                behavior: 'smooth'
-            })
-        }
-    })
-})
+      window.scrollTo({
+        top: targetOffset,
+        behavior: "smooth",
+      });
+    }
+  });
+});
 
 window.addEventListener("scroll", () => {
-    const barraMenu = document.querySelector('.menu-topo__fundo');
-    const scrollY = window.scrollY
+  const barraMenu = document.querySelector(".menu-topo__fundo");
+  const scrollY = window.scrollY;
 
-    if(scrollY < 50) {
-        barraMenu.style.transform = "translateY(-100%)"
-    } else {
-        barraMenu.style.transform = "translateY(0)"
-    }
-})
+  if (scrollY < 50) {
+    barraMenu.style.transform = "translateY(-100%)";
+  } else {
+    barraMenu.style.transform = "translateY(0)";
+  }
+});
