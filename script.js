06d1cb33706e6153fd59757294b6d4a771296944
gabrielbller <gabrielbller@icloud.com>
2023-11-27 16:07:00 -0300

    Melhorias

--- script.js
+++ script.js
@@ -8,48 +8,85 @@ document.getElementById("loadMore").addEventListener("click", function () {
   }
 });
 
-const menuAbrir = document.querySelector(".menu-barras");
-
-const nav = document.querySelector(".cabecalho");
-
-menuAbrir.addEventListener("click", () => {
-  menuAbrir.classList.toggle("aberto");
-  nav.classList.toggle("aberto");
+// ---------Responsive-navbar-active-animation-----------
+function test() {
+  var tabsNewAnim = $("#navbarSupportedContent");
+  var selectorNewAnim = $("#navbarSupportedContent").find("li").length;
+  var activeItemNewAnim = tabsNewAnim.find(".active");
+  var activeWidthNewAnimHeight = activeItemNewAnim.innerHeight();
+  var activeWidthNewAnimWidth = activeItemNewAnim.innerWidth();
+  var itemPosNewAnimTop = activeItemNewAnim.position();
+  var itemPosNewAnimLeft = activeItemNewAnim.position();
+  $(".hori-selector").css({
+    top: itemPosNewAnimTop.top + "px",
+    left: itemPosNewAnimLeft.left + "px",
+    height: activeWidthNewAnimHeight + "px",
+    width: activeWidthNewAnimWidth + "px",
+  });
+  $("#navbarSupportedContent").on("click", "li", function (e) {
+    $("#navbarSupportedContent ul li").removeClass("active");
+    $(this).addClass("active");
+    var activeWidthNewAnimHeight = $(this).innerHeight();
+    var activeWidthNewAnimWidth = $(this).innerWidth();
+    var itemPosNewAnimTop = $(this).position();
+    var itemPosNewAnimLeft = $(this).position();
+    $(".hori-selector").css({
+      top: itemPosNewAnimTop.top + "px",
+      left: itemPosNewAnimLeft.left + "px",
+      height: activeWidthNewAnimHeight + "px",
+      width: activeWidthNewAnimWidth + "px",
+    });
+  });
+}
+$(document).ready(function () {
+  setTimeout(function () {
+    test();
+  });
 });
-
-const botoes = document.querySelectorAll(".cabecalho__botao");
-
-const botoesClicavel = document.querySelectorAll(".cabecalho__botao-clicavel");
-
-botoesClicavel.forEach((botao) => {
-  botao.addEventListener("click", (event) => {
-    event.preventDefault();
-
-    const targetId = botao.getAttribute("href");
-    const targetElement = document.querySelector(targetId);
-
-    if (targetElement) {
-      const targetOffset =
-        targetElement.getBoundingClientRect().top + window.scrollY;
-
-      window.scrollTo({
-        top: targetOffset,
-        behavior: "smooth",
-      });
-    }
+$(window).on("resize", function () {
+  setTimeout(function () {
+    test();
+  }, 500);
+});
+$(".navbar-toggler").click(function () {
+  $(".navbar-collapse").slideToggle(300);
+  setTimeout(function () {
+    test();
   });
 });
 
-window.addEventListener("scroll", () => {
-  const barraMenu = document.querySelector(".menu-topo__fundo");
-  const scrollY = window.scrollY;
+// --------------add active class-on another-page move----------
+jQuery(document).ready(function ($) {
+  // Get current path and find target link
+  var path = window.location.pathname.split("/").pop();
 
-  if (scrollY < 50) {
-    barraMenu.style.transform = "translateY(-100%)";
-  } else {
-    barraMenu.style.transform = "translateY(0)";
+  // Account for home page with empty path
+  if (path == "") {
+    path = "index.html";
   }
+
+  var target = $('#navbarSupportedContent ul li a[href="' + path + '"]');
+  // Add active class to target link
+  target.parent().addClass("active");
 });
+
+// Add active class on another page linked
+// ==========================================
+// $(window).on('load',function () {
+//     var current = location.pathname;
+//     console.log(current);
+//     $('#navbarSupportedContent ul li a').each(function(){
+//         var $this = $(this);
+//         // if the current path is like this link, make it active
+//         if($this.attr('href').indexOf(current) !== -1){
+//             $this.parent().addClass('active');
+//             $this.parents('.menu-submenu').addClass('show-dropdown');
+//             $this.parents('.menu-submenu').parent().addClass('active');
+//         }else{
+//             $this.parent().removeClass('active');
+//         }
+//     })
+// });
 
 class FormSubmit {
   constructor(configs) {
